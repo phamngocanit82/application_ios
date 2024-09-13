f1138e4ae54a83c4351e5c8f260d0e7607f51942
phamngocanit82 <ngocanphamit271082@gmail.com>
2024-09-13 12:31:18 +0700

    add new application_ios

---
+++ application_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  application_ios
+//
+//  Created by An Pham Ngoc on 9/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
